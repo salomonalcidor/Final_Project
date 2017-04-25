99b200c7f3926e2051571c31fd3ce2f29aa64ef9
Salomon Alcidor <salomon.alcidor@gmail.com>
2017-04-24 20:38:57 -0400

    Revamped Project

---
+++ app/scripts/main.js
@@ -0,0 +1,10 @@
+// Activate Fullpage
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+	    sectionsColor: ['#327695', '#DA90D9', '#C2BAE8', '#70FF99', '#FF14B1'],
+	    anchors:['Page1', 'Page2', 'Page3', 'Page4', 'Page5'],
+	    menu: '#Menu',
+	    scrollingSpeed: 1000
+
+	});
+});
